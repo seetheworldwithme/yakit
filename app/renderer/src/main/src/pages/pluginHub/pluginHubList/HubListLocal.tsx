5e5830592b616cb44bd3e9e321a43e2c135ef66c
听花开了 <xuziyuesir@163.com>
2026-07-07 10:36:34 +0800

    fix(pluginHub): 修复插件表格分页滚动

--- app/renderer/src/main/src/pages/pluginHub/pluginHubList/HubListLocal.tsx
+++ app/renderer/src/main/src/pages/pluginHub/pluginHubList/HubListLocal.tsx
@@ -1472,7 +1472,7 @@ export const HubListLocal: React.FC<HubListLocalProps> = memo((props) => {
                         pageSizeOptions: ['10', '20', '50', '100'],
                         onChange: onPaginationChange,
                       }}
-                      scroll={{ y: 200, x: 'max-content' }}
+                      scroll={{ y: 'calc(100vh - 360px)', x: 'max-content' }}
                       loading={loading}
                       onRow={(record) => {
                         const idx = (response.Data || []).findIndex((ele) => ele.ScriptName === record.ScriptName)
--- app/renderer/src/main/src/pages/pluginHub/pluginHubList/HubListOwn.tsx
+++ app/renderer/src/main/src/pages/pluginHub/pluginHubList/HubListOwn.tsx
@@ -861,7 +861,7 @@ export const HubListOwn: React.FC<HubListOwnProps> = memo((props) => {
                           pageSizeOptions: ['10', '20', '50', '100'],
                           onChange: onPaginationChange,
                         }}
-                        scroll={{ y: 200, x: 'max-content' }}
+                        scroll={{ y: 'calc(100vh - 360px)', x: 'max-content' }}
                         loading={loading}
                         onRow={(record) => {
                           const idx = (response.data || []).findIndex((ele) => ele.uuid === record.uuid)
