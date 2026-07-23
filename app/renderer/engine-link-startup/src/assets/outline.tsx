eced389ae002d9fc73b0fb2c0bc5988934f67634
听花开了 <xuziyuesir@163.com>
2026-07-23 15:32:52 +0800

    fix startup warning icon export

--- app/renderer/engine-link-startup/src/assets/outline.tsx
+++ app/renderer/engine-link-startup/src/assets/outline.tsx
@@ -104,6 +104,25 @@ export const OutlineArrowcirclerightIcon = (props: Partial<IconProps>) => {
   return <Icon component={OutlineArrowcircleright} {...props} />
 }
 
+const OutlineExclamationcircle = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 8V12M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <circle cx="12" cy="16" r="1" fill="currentColor" />
+  </svg>
+)
+/**
+ * @description Icon/Outline/Outlineexclamation-circle
+ */
+export const OutlineExclamationcircleIcon = (props: Partial<IconProps>) => {
+  return <Icon component={OutlineExclamationcircle} {...props} />
+}
+
 const OutlineExit = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
